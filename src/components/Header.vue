a06264429dda609248f70748216038f1343958ad
Omar Labana <omarlabana@outlook.com>
2021-05-16 18:34:25 +0200

    Header cleanup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -30,36 +30,9 @@
         <li><a href="#" class="px-2 py-5 hover:bg-gray-100">Contact</a></li>
       </ul>
       <!-- Mbile Menu -->
-      <div class="md:hidden">
-        <button
-          type="button"
-          class="inline-flex items-center justify-center p-2 rounded-md text-pink-800 hover:text-white hover:bg-pink-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="block h-8 w-8"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-      </div>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
 </script>
-
-<style>
-</style>
