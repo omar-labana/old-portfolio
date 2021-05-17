b59bed5dc216b4bbc0ab0bd2fe7f091842d770ee
Omar Labana <omarlabana@outlook.com>
2021-05-17 18:57:29 +0200

    Add one static project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
   <Hero />
+  <Projects />
 </template>
 
 <script setup>
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
+import Projects from "./components/Projects.vue";
 </script>
 
---
+++ src/components/Projects.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <h2 class="text-4xl font-semibold">Projects</h2>
+    <!-- Cards -->
+    <div class="my-20 bg-indigo-50 border rounded-xl p-5 ">
+      <!-- Image -->
+      <div class="px-5 py-7 rounded-xl bg-indigo-400">
+        <img src="../assets/100H.png" alt="" class="">
+      </div>
+      <!-- Description -->
+      <div>
+        <h3 class="text-xl font-bold my-2">100H Sketch</h3>
+      <ul class="flex  font-semibold text-gray-500">
+        <li class="mr-3 text-gray-800">Personal</li>
+        <li class="mr-2 ">•</li>
+        <li class="mr-3 ">Full Stack</li>
+        <li class="mr-2 ">•</li>
+        <li>2020</li>
+      </ul>
+      <p>Shared sketch between players, one player can draw while others guess the drawing.</p>
+      <ul class="flex flex-wrap text-indigo-900 items-center text-xs">
+        <li class="mr-2 bg-indigo-200 px-2 py-1 my-1 rounded">HTML5/CSS3</li>
+        <li class="mr-2 bg-indigo-200 px-2 py-1 my-1 rounded">Node.js</li>
+        <li class="mr-2 bg-indigo-200 px-2 py-1 my-1 rounded">Express.js</li>
+        <li class="mr-2 bg-indigo-200 px-2 py-1 my-1 rounded">Socket.io</li>
+        <li class="mr-2 bg-indigo-200 px-2 py-1 my-1 rounded">p5.js</li>
+      </ul>
+      <button class="px-2 py-1 mt-2 border-2 border-blue-900 rounded-md  text-blue-900 font-semibold">See Project</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+const projects = [
+
+]
+export default {
+
+}
+</script>
