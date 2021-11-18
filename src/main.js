3dcc7cc4f5e9db11f45b2f8a0b15b59ff02321c6
Omar Labana <omarlabana@outlook.com>
2021-11-18 13:42:33 +0200

    Deploy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Icon from "./components/Icon.vue"
-import './index.css'
-
-const app = createApp(App)
-
-app.component('Icon', Icon)
-app.mount('#app')
